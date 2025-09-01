9c67bb6bfeb76ccc3c0c70b0ff87d213e340a7a8
TheNorn <Quanxinzhou@iClooud.com>
2025-09-01 11:02:10 +0800

    chore: 强制覆盖上传所有文件

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.chinese_herb_framery"
+    namespace = "com.yourdomain.chineseHerbfragmemoryAppThenornbuild"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.chinese_herb_framery"
+        applicationId = "com.yourdomain.chineseHerbfragmemoryAppThenornbuild"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
